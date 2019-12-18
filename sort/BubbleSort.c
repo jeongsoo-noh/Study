564c742c02c8437e3bbe22ce6ec0f1bc71ea3c9f
jeongsoo <jeongsoo.dev@gmail.com>
2019-12-18 21:31:51 +0900

    Remove #if statements

--- sort/BubbleSort.c
+++ sort/BubbleSort.c
@@ -1,32 +1,18 @@
 #include <stdio.h>
 
-#define ASCENDING	1
-#define DESCENDING	2
-
-#define ORDER		ASCENDING
-
+// Ascending
 void BubbleSort(int arr[], int n)
 {
 	for (int i = 0; i < n - 1; i++)
 	{
 		for (int j = 0; j < n - 1 - i; j++)
 		{
-#if ORDER == ASCENDING
 			if (arr[j] > arr[j + 1])
 			{
 				int tmp = arr[j];
 				arr[j] = arr[j + 1];
 				arr[j + 1] = tmp;
 			}
-#endif
-#if ORDER == DESCENDING
-			if (arr[j] < arr[j + 1])
-			{
-				int tmp = arr[j];
-				arr[j] = arr[j + 1];
-				arr[j + 1] = tmp;
-			}
-#endif
 		}
 	}
 }
